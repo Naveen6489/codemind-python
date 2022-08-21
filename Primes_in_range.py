669655843df8a42bd8ef253f1f8d42906dc586cd
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-08-21 09:19:17 +0530

    def prime(n):     k=n**0.5     for i in range(2,int(k)+1):         if n%i==0:             return False     else:         return True n=int(input()) m=int(input()) c=0 for i in range(n,m+1):     if i<=1:         continue
        if prime(i):
            c+=1
    print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,20 @@
+def prime(n):
+    k=n**0.5
+    for i in range(2,int(k)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+m=int(input())
+c=0
+for i in range(n,m+1):
+    if i<=1:
+        continue
+        
+    if prime(i):
+        c+=1
+print(c)
+        
+            
+        
