a9193128dc7ab1a3d0c64ff70b5f0ee9a0d2f8f7
Naveen6489 <102216712+Naveen6489@users.noreply.github.com>
2022-11-08 11:05:25 +0530

    n=input() d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000} s=d[n[-1]] for i in range(len(n)-1):     if(d[n[i]]<d[n[i+1]]):         s-=d[n[i]]     else:         s+=d[n[i]] print(s)

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,11 +1,9 @@
-s=input()
+n=input()
 d={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-c=d[s[-1]]
-
-for i in range(len(s)-1):
-            if d[s[i]]<d[s[i+1]]:
-                c-=d[s[i]]
-            else:
-                c+=d[s[i]]
-print(c)
-        
+s=d[n[-1]]
+for i in range(len(n)-1):
+    if(d[n[i]]<d[n[i+1]]):
+        s-=d[n[i]]
+    else:
+        s+=d[n[i]]
+print(s)
